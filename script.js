ee441d2ca6b4775ad30c8f003027cdb755ad2851
Oana Pintoiu <oana.pintoiu@yahoo.com>
2023-07-20 13:08:32 +0100

    added scroll down feature on welcome-intro and divided sections

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-document.addEventListener("DOMContentLoaded", function () {
+// Traits words transition on welcome-intro section
+
+document.addEventListener("DOMContentLoaded", () => {
   const dynamicText = document.getElementById("dynamicText");
   const words = ["Confident", "Creative", "Ambitious"];
   const interval = 3000; // Change the interval (in milliseconds) between word transitions
@@ -6,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   function changeWord() {
     dynamicText.style.opacity = 0; // Fade out
-    setTimeout(function () {
+    setTimeout(() => {
       dynamicText.textContent = words[currentWordIndex];
       dynamicText.style.opacity = 1; // Fade in
 
@@ -19,4 +21,38 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   changeWord();
+});
+
+
+// Scroll down feature
+const scrollDownLink = document.getElementById('scroll-down-link');
+const aboutSection = document.getElementById('about');
+
+const handleIntersection = (entries) => {
+  const [entry] = entries;
+  if (entry.isIntersecting) {
+    scrollDownLink.classList.add('hidden'); // Apply the hidden class
+  } else {
+    scrollDownLink.classList.remove('hidden'); // Remove the hidden class
+  }
+};
+
+scrollDownLink.addEventListener('click', () => {
+  scrollDownLink.classList.add('hidden');
+});
+
+const observer = new IntersectionObserver(handleIntersection, {
+  threshold: 0.15,
+});
+
+observer.observe(aboutSection);
+
+// Scroll behavior
+scrollDownLink.addEventListener('click', (event) => {
+  event.preventDefault();
+  const targetSection = document.querySelector(scrollDownLink.getAttribute('href'));
+
+  targetSection.scrollIntoView({
+    behavior: 'smooth',
+  });
 });
