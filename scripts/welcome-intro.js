2214c8d616179ada376285dd41ce05c0ce06a754
Oana Pintoiu <oana.pintoiu@yahoo.com>
2023-07-21 22:29:02 +0100

    added next/previous buttons for dynamic text for welcome-intro section & removed most study notes

--- scripts/welcome-intro.js
+++ scripts/welcome-intro.js
@@ -1,8 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   const dynamicText = document.getElementById("dynamicText");
+  const nextButton = document.getElementById("nextButton");
+  const prevButton = document.getElementById("prevButton");
 
-  const words = ["Confident", "Creative", "Ambitious"];
-  const interval = 3000; // Change the interval (in milliseconds) between word transitions
+  const words = ["Welcome to my Website.", "I am a Web Developer.", "Resourceful Project Manager.", "Experienced Transfomational Leader.","Passionate about People, Business and Tech.", "Caught your attention?", "Good.", "Scroll on Down to find out More."];
+  const interval = 3000; // change the interval (in milliseconds) between word transitions
   let currentWordIndex = 0;
 
   const changeWord = () => {
@@ -16,8 +18,26 @@ document.addEventListener("DOMContentLoaded", () => {
         currentWordIndex = 0;
       }
       setTimeout(changeWord, interval);
-    }, 500); // Adjust the duration (in milliseconds) for the fade-out effect
+    }, 500); 
   };
+
+  prevButton.addEventListener("click", () => {
+    currentWordIndex = (currentWordIndex - 1 + words.length) % words.length; // decrements 'currentWordIndex' variable by 1 meaning it wraps around the last word of the 'words' array
+    dynamicText.style.opacity = 0;
+    setTimeout(() => {
+      dynamicText.textContent = words[currentWordIndex];
+      dynamicText.style.opacity = 1; 
+    }, 500);
+  });
+
+  nextButton.addEventListener("click", () => {
+    currentWordIndex = (currentWordIndex + 1) % words.length;
+    dynamicText.style.opacity = 0;
+    setTimeout(() => {
+      dynamicText.textContent = words[currentWordIndex];
+      dynamicText.style.opacity = 1; 
+    }, 500);
+  });
 
   changeWord();
 
@@ -26,15 +46,33 @@ document.addEventListener("DOMContentLoaded", () => {
   const handleIntersection = (entries) => {
     const [entry] = entries;
     if (entry.isIntersecting) {
-      dynamicText.classList.add("hidden"); // Apply the hidden class
+      dynamicText.classList.add("hidden"); 
     } else {
-      dynamicText.classList.remove("hidden"); // Remove the hidden class
+      dynamicText.classList.remove("hidden"); 
+      nextButton.classList.remove("hidden");
+      prevButton.classList.remove("hidden");
     }
   };
 
+  const handleButtonIntersection = (entries) => {
+    const [entry] = entries;
+    if (entry.isIntersecting) {
+      nextButton.classList.add("hidden");
+      prevButton.classList.add("hidden");
+    } else {
+      nextButton.classList.remove("hidden");
+      prevButton.classList.remove("hidden");
+    }
+  }
+
+ 
   const observer = new IntersectionObserver(handleIntersection, {
     threshold: 0.15,
   });
+  const buttonObserver = new IntersectionObserver (handleButtonIntersection, {
+    threshold: 0.1278,
+  })
 
   observer.observe(aboutSection);
+  buttonObserver.observe(aboutSection);
 });
