e0b26e2665c0b5cd4ffb36af78d0d04888bb25ba
Oana Pintoiu <oana.pintoiu@yahoo.com>
2023-08-10 14:49:43 +0100

    performed code formatting

--- scripts/skills_section.js
+++ scripts/skills_section.js
@@ -1,46 +1,48 @@
-const skillBars = document.querySelectorAll(".bar span");
-let skillSectionVisible = false;
+document.addEventListener("DOMContentLoaded", () => {
+  const skillBars = document.querySelectorAll(".bar span");
+  let skillSectionVisible = false;
 
-const animateSkillBars = () => {
-  skillBars.forEach((bar) => {
-    const width = bar.dataset.width;
-    bar.style.width = width;
+  const animateSkillBars = () => {
+    skillBars.forEach((bar) => {
+      const width = bar.dataset.width;
+      bar.style.width = width;
+    });
+  };
+
+  const unfillSkillBars = () => {
+    skillBars.forEach((bar) => {
+      bar.style.width = "0";
+    });
+  };
+
+  const handleIntersection = (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        skillSectionVisible = true;
+        animateSkillBars();
+      } else {
+        skillSectionVisible = false;
+        unfillSkillBars();
+      }
+    });
+  };
+
+  const skillObserver = new IntersectionObserver(handleIntersection, {
+    threshold: 0.2,
   });
-};
 
-const unfillSkillBars = () => {
-  skillBars.forEach((bar) => {
-    bar.style.width = "0";
-  });
-};
+  const skillSection = document.querySelector(".skill-type");
+  skillObserver.observe(skillSection);
 
-const handleIntersection = (entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      skillSectionVisible = true;
-      animateSkillBars();
-    } else {
-      skillSectionVisible = false;
-      unfillSkillBars();
+  const handleScroll = () => {
+    if (!skillSectionVisible) {
+      const rect = skillSection.getBoundingClientRect();
+      if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+        skillSectionVisible = true;
+        animateSkillBars();
+      }
     }
-  });
-};
+  };
 
-const skillObserver = new IntersectionObserver(handleIntersection, {
-  threshold: 0.2,
+  window.addEventListener("scroll", handleScroll);
 });
-
-const skillSection = document.querySelector(".skill-type");
-skillObserver.observe(skillSection);
-
-const handleScroll = () => {
-  if (!skillSectionVisible) {
-    const rect = skillSection.getBoundingClientRect();
-    if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
-      skillSectionVisible = true;
-      animateSkillBars();
-    }
-  }
-};
-
-window.addEventListener("scroll", handleScroll);
