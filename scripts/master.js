87284580282a7813d2119d721e92c9511fa2c6d1
Oana Pintoiu <oana.pintoiu@yahoo.com>
2023-08-10 13:43:05 +0100

    populated skills section and updated file names for readability

--- scripts/master.js
+++ scripts/master.js
@@ -1,3 +1,4 @@
 document.write('<script src="scripts/welcome-intro.js"></script>');
 document.write('<script src="scripts/scroll-up-down.js"></script>');
-document.write('<script src="scripts/nav.js"></script>');
+document.write('<script src="scripts/nav_bar.js"></script>');
+document.write('<script src="scripts/skills_section.js"></script>');
---
+++ scripts/skills_section.js
@@ -0,0 +1,46 @@
+const skillBars = document.querySelectorAll(".bar span");
+let skillSectionVisible = false;
+
+const animateSkillBars = () => {
+  skillBars.forEach((bar) => {
+    const width = bar.dataset.width;
+    bar.style.width = width;
+  });
+};
+
+const unfillSkillBars = () => {
+  skillBars.forEach((bar) => {
+    bar.style.width = "0";
+  });
+};
+
+const handleIntersection = (entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      skillSectionVisible = true;
+      animateSkillBars();
+    } else {
+      skillSectionVisible = false;
+      unfillSkillBars();
+    }
+  });
+};
+
+const skillObserver = new IntersectionObserver(handleIntersection, {
+  threshold: 0.2,
+});
+
+const skillSection = document.querySelector(".skill-type");
+skillObserver.observe(skillSection);
+
+const handleScroll = () => {
+  if (!skillSectionVisible) {
+    const rect = skillSection.getBoundingClientRect();
+    if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+      skillSectionVisible = true;
+      animateSkillBars();
+    }
+  }
+};
+
+window.addEventListener("scroll", handleScroll);
