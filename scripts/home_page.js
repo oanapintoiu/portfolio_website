585e581e67150f47bb505c33c5d542169a50bf57
Oana Pintoiu <oana.pintoiu@yahoo.com>
2023-09-10 16:35:14 +0300

    auto navigation now functional

---
+++ scripts/home_page.js
@@ -0,0 +1,25 @@
+const radioButtons = document.querySelectorAll('input[name="images"]');
+let counter = 0;
+let intervalId; // Variable to store the interval ID
+
+// Function to handle changing the radio button and restarting the interval
+function changeRadioButton() {
+  clearInterval(intervalId); // 
+  radioButtons[counter].checked = false;
+  counter = (counter + 1) % radioButtons.length;
+  radioButtons[counter].checked = true;
+  intervalId = setInterval(changeRadioButton, 4000); // Start a new interval
+}
+
+// Add a click event listener to each radio button
+radioButtons.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    clearInterval(intervalId); // 
+    counter = index; // Set the counter to the clicked button's index
+    radioButtons[counter].checked = true; 
+    intervalId = setInterval(changeRadioButton, 4000); 
+  });
+});
+
+// Start the initial interval
+intervalId = setInterval(changeRadioButton, 4000);
