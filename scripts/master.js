5142b46dbf5d53f4ab842de8c8d2f9825e2ddcf8
Oana Pintoiu <oana.pintoiu@yahoo.com>
2024-01-24 11:48:54 +0000

    added contact section template and updated .css for body/all sections with a fixed background images; changed colours of font to fit new background

--- scripts/master.js
+++ scripts/master.js
@@ -3,3 +3,4 @@ document.write('<script src="scripts/scroll_up_down.js"></script>');
 document.write('<script src="scripts/nav_bar.js"></script>');
 document.write('<script src="scripts/skills_section.js"></script>');
 document.write('<script src="scripts/menu_box.js"></script>');
+document.write('<script src="scripts/contact.js"></script>');
