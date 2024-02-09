0a1f92ed266c201ab10bf7663806324641fede40
Oana Pintoiu <oana.pintoiu@yahoo.com>
2024-02-09 12:44:58 +0000

    restyled portfolio, contact and navbar to fit the rest of the page; updated data on experience bars in html; updated content in experience section; deleted more redundant code; fixed errors in terminal.

--- scripts/master.js
+++ scripts/master.js
@@ -1,6 +1,4 @@
-document.write('<script src="scripts/home_page.js"></script>');
 document.write('<script src="scripts/scroll_up_down.js"></script>');
 document.write('<script src="scripts/nav_bar.js"></script>');
 document.write('<script src="scripts/skills_section.js"></script>');
 document.write('<script src="scripts/menu_box.js"></script>');
-document.write('<script src="scripts/contact.js"></script>');
