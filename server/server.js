e4805395a9b1de394a0307083504c5ec7295125a
Oana Pintoiu <oana.pintoiu@yahoo.com>
2024-08-08 15:26:54 +0100

    added redirection after form submission

--- server/server.js
+++ server/server.js
@@ -2,9 +2,12 @@ const express = require('express');
 const axios = require('axios');
 const bodyParser = require('body-parser');
 require('dotenv').config();
+const path = require('path');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, '../'))); 
 
 app.post('/submit', async (req, res) => {
     try {
@@ -12,11 +15,58 @@ app.post('/submit', async (req, res) => {
             ...req.body,
             access_key: process.env.WEB3FORMS_API_KEY
         });
-        res.status(200).send('Form Submitted Successfully');
+        res.redirect('/redirect-success');
     } catch (error) {
-        console.error(error);
-        res.status(500).send('Oops! Something went wrong, please try again.');
+        console.error('Error details:', {
+            message: error.message,
+            response: error.response ? error.response.data : 'No response data',
+            config: error.config
+        });
+        res.redirect('/redirect-error');
     }
+});
+
+// Route for successful redirection
+app.get('/redirect-success', (req, res) => {
+    res.status(200).send(`
+        <html>
+            <head>
+                <title>Submission Successful</title>
+            </head>
+            <body>
+                <p>Form Submitted Successfully. You will be redirected shortly.</p>
+                <script>
+                    setTimeout(function() {
+                        window.location.href = '/';
+                    }, 3000);
+                </script>
+            </body>
+        </html>
+    `);
+});
+
+// Route for error redirection
+app.get('/redirect-error', (req, res) => {
+    res.status(500).send(`
+        <html>
+            <head>
+                <title>Error</title>
+            </head>
+            <body>
+                <p>Oops! Something went wrong. Please try again.</p>
+                <script>
+                    setTimeout(function() {
+                        window.history.back();
+                    }, 3000);
+                </script>
+            </body>
+        </html>
+    `);
+});
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
